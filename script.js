a85fb9646fcbef96207659bdc206c263c2c69ae8
Aniket Nayek <nayekaniket2017@gmail.com>
2021-12-30 09:55:45 +0530

    Fourth Commit

--- script.js
+++ script.js
@@ -4,13 +4,15 @@ function getData()
 {
     city = document.querySelector(".search-box").value;
     api = "f74dabb01c9ee65a2b1f4d31e90a53ef"
-    url = "api.openweathermap.org/data/2.5/weather?q="+this.city+"&appid="+this.api;
-    /*fetch(url).then((response)=>{
-        response.json();
+    url = "https://api.openweathermap.org/data/2.5/weather?q="+this.city+"&units=metric&appid="+this.api;
+    fetch(url).then((response)=>{
+        return response.json();
     }).then((data)=>{
-        console.log(response);
-    })*/
-    console.log(this.city);
+        document.querySelector(".dec").innerText = data.weather[0].description;
+        document.querySelector(".city").innerText = "Weather in " + data.name;
+        document.querySelector(".icon").src =
+        "https://openweathermap.org/img/wn/" + data.weather[0].icon + ".png";
+    })
 }
 
 document.querySelector(".searchimg").addEventListener("click", function () {
