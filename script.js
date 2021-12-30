18080f50c000c1c3a32cdae44a7b3b6ecfcdc7a0
Aniket Nayek <nayekaniket2017@gmail.com>
2021-12-30 10:15:59 +0530

    Fifth commit / complete working

--- script.js
+++ script.js
@@ -6,12 +6,22 @@ function getData()
     api = "f74dabb01c9ee65a2b1f4d31e90a53ef"
     url = "https://api.openweathermap.org/data/2.5/weather?q="+this.city+"&units=metric&appid="+this.api;
     fetch(url).then((response)=>{
+        if (!response.ok) {
+            alert("No weather found.");
+            throw new Error("No weather found.");
+          }
         return response.json();
     }).then((data)=>{
         document.querySelector(".dec").innerText = data.weather[0].description;
         document.querySelector(".city").innerText = "Weather in " + data.name;
         document.querySelector(".icon").src =
         "https://openweathermap.org/img/wn/" + data.weather[0].icon + ".png";
+        document.querySelector(".temp").innerText = data.main.temp + "°C";
+        /*temp1 = data.main.temp;
+        if(temp1>20)
+            document.body.style.backgroundImage = "url('./Images/hot')"*/
+        document.querySelector(".humid").innerText = "Humidity : "+ data.main.humidity+"%";
+        document.querySelector(".wind").innerText = "Wind : " + data.wind.speed + "km/h";
     })
 }
 
